013b5ce5c76dd96d219fb08fe34e4f34d77bd8ae
MariaPoala <maria.vilcapoma22@gmail.com>
2024-10-30 07:18:52 -0500

    cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainComponent },
+    { path: 'nosotros', component: NosotrosComponent }
+    // { path: 'servicios', component: ServiciosComponent },
+    // { path: 'proyectos', component: ProyectosComponent },
+    // { path: 'contacto', component: ContactoComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
